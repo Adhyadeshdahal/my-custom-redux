dc21c716bb380acc0bbb8fcea0a02e10c3a5c380
Adhyadeshdahal <adhyadeshdahal618@gmail.com>
2023-07-17 19:19:40 +0545

    added getState,dispatch and subscribe fn

--- store.js
+++ store.js
@@ -1,4 +1,31 @@
-function createStore(){
+function createStore(reducer){
+    let state;
+    let subscribeFn = ()=>{null};
 
-    
+    function getState(){
+        return state;
+    };
+
+    function dispatch(action){
+        //call reducer
+      state = [...reducer(state,action)];
+      subscribeFn();
+        //call subscriber
+    };
+
+    function unSubscribe(){
+        subscribeFn= ()=>{null};
+    };
+
+    function subscribe(fn){
+        subscribeFn=fn;
+        return unSubscribe;
+    }
+
+    return {
+        getState,
+        dispatch,
+        subscribe
+    }
+
 }
